389dd6d08426e7889ee80824475d42efc4251bbe
Austin Cheng <austinbicheng@gmail.com>
2018-01-30 09:11:14 -0800

    cards working?

---
+++ New Unity Project/Assets/Draggable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler {
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("hello");
+        Debug.Log(eventData.position.x);
+        Debug.Log(eventData.position.y);
+        Debug.Log(transform.position);
+        transform.position = eventData.position;
+    }
+}
